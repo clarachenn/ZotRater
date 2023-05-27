29545026eb5a224f64b5d12515effa1648b2fa10
maitreyi03 <96412244+maitreyi03@users.noreply.github.com>
2023-05-27 07:30:36 -0700

    Added the ratings file for ZotoMeter interface

---
+++ script.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var sliders = document.getElementsByClassName('slider');
+
+    Array.prototype.forEach.call(sliders, function (slider) {
+        var sliderHandle = slider.querySelector('.sliderHandle');
+        var sliderBackground = slider.querySelector('.sliderBackground');
+        var emoji = slider.nextElementSibling;
+
+        sliderHandle.addEventListener('mousedown', function (event) {
+            event.preventDefault();
+            document.addEventListener('mousemove', moveSlider);
+            document.addEventListener('mouseup', stopSlider);
+        });
+
+        function moveSlider(event) {
+            var position = event.clientX - slider.getBoundingClientRect().left;
+            var percentage = (position / slider.offsetWidth) * 100;
+            percentage = Math.max(0, Math.min(100, percentage));
+
+            sliderHandle.style.left = percentage + '%';
+            sliderBackground.style.width = percentage + '%';
+
+            var rating = Math.round((percentage / 100) * 5);
+            updateEmoji(rating);
+        }
+
+        function stopSlider() {
+            document.removeEventListener('mousemove', moveSlider);
+            document.removeEventListener('mouseup', stopSlider);
+        }
+
+        function updateEmoji(rating) {
+            if (rating < 2.5) {
+                emoji.innerHTML = '😢';
+            } else if (rating >= 2.5 && rating <= 3.4) {
+                emoji.innerHTML = '😐';
+            } else {
+                emoji.innerHTML = '😄';
+            }
+        }
+    });
+});
